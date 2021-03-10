f61f95a823eea85927725392ee963acb51ca2258
Yakobus Geganaseta <yakobus.geganaseta@sinergymediaintegrasi.com>
2021-03-10 12:26:13 +0700

    Add project files.

---
+++ UserRegistration/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UserRegistration.Models;
+
+namespace UserRegistration
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserRegistrationModel, User>()
+                .ForMember(u => u.UserName, opt => opt.MapFrom(x => x.Email));
+        }
+    }
+}
---
+++ UserRegistration/Models/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using UserRegistration.Models.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserRegistration.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
